5efaf6b653e0835df1a6efd3474cbd498f75427b
Anmol Sinha <anmolrsinha@gmail.com>
2024-09-21 11:02:30 +0530

    Initial commit

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,19 +1,38 @@
 const Channels = ({ provider, account, dappcord, channels, currentChannel, setCurrentChannel }) => {
+  const channelHandler = async (channel) => {
+
+    const hasJoined = await dappcord.hasJoined(channel.id, account)
+
+    if (hasJoined) {
+      setCurrentChannel(channel)
+    } else {
+      const signer = await provider.getSigner()
+      const transaction = await dappcord.connect(signer).mint(channel.id, { value: channel.cost })
+      await transaction.wait()
+      setCurrentChannel(channel)
+    }
+  }
 
   return (
     <div className="channels">
       <div className="channels__text">
-        <h2>Text Channels</h2>
+        <h2>Departments</h2>
 
+        <ul>
+          {channels.map((channel, index) => (
+            <li
+              onClick={() => channelHandler(channel)} key={index}
+              className={currentChannel && currentChannel.id.toString() === channel.id.toString() ? "active" : ""}>
+              {channel.name}
+            </li>
+          ))}
+        </ul>
       </div>
 
       <div className="channels__voice">
-        <h2>Voice Channels</h2>
+        
 
         <ul>
-          <li>Channel 1</li>
-          <li>Channel 2</li>
-          <li>Channel 3</li>
         </ul>
       </div>
     </div>
@@ -21,3 +40,50 @@ const Channels = ({ provider, account, dappcord, channels, currentChannel, setCu
 }
 
 export default Channels;
+
+// import React from 'react';
+
+// const Channels = ({ provider, account, dappcord, channels, currentChannel, setCurrentChannel }) => {
+//   const channelHandler = async (channel) => {
+//     const hasJoined = await dappcord.hasJoined(channel.id, account);
+
+//     if (hasJoined) {
+//       setCurrentChannel(channel);
+//     } else {
+//       const signer = await provider.getSigner();
+//       const transaction = await dappcord.connect(signer).mint(channel.id, { value: channel.cost });
+//       await transaction.wait();
+//       setCurrentChannel(channel);
+//     }
+//   };
+
+  
+//   const filteredChannels = channels.filter(channel => ["general"].includes(channel.name));
+//   console.log(filteredChannels);
+
+//   return (
+//     <div className="channels">
+//       <div className="channels__text">
+//         <h2>Departments</h2>
+
+//         <ul>
+//           {filteredChannels.map((channel, index) => (
+//             <li
+//               onClick={() => channelHandler(channel)}
+//               key={index}
+//               className={currentChannel && currentChannel.id.toString() === channel.id.toString() ? "active" : ""}
+//             >
+//               {channel.name}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+
+//       <div className="channels__voice">
+//         <ul>{""}</ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Channels;
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,24 +1,84 @@
-import { useEffect, useState, useRef } from 'react'
-import { io } from "socket.io-client"
+import { useEffect, useState, useRef } from 'react';
+import { io } from 'socket.io-client';
 
 // Assets
-import person from '../assets/person.svg'
-import send from '../assets/send.svg'
+import person from '../assets/person.svg';
+import send from '../assets/send.svg';
 
 // Socket
-const socket = io('ws://localhost:3030')
+const socket = io('ws://localhost:3030');
 
 const Messages = ({ account, messages, currentChannel }) => {
+  const [message, setMessage] = useState('');
+
+  const messageEndRef = useRef(null);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const startTime = performance.now(); // Get start time before sending message
+
+    const messageObj = {
+      channel: currentChannel.id.toString(),
+      account: account,
+      text: message,
+    };
+
+    if (message !== '') {
+      socket.emit('new message', messageObj);
+    }
+
+    const endTime = performance.now(); 
+    const executionTime = endTime - startTime; 
+
+    console.log('Blockchain execution time:', executionTime); 
+
+    setMessage('');
+  };
+
+  const scrollHandler = () => {
+    setTimeout(() => {
+      messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }, 500);
+  };
+
+  useEffect(() => {
+    scrollHandler();
+  });
+
+  const isAdminMessage = (message) => {
+    return message.account === '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'; 
+  };
 
   return (
     <div className="text">
       <div className="messages">
-
+        {currentChannel &&
+          messages
+            .filter((message) => message.channel === currentChannel.id.toString())
+            .map((message, index) => (
+              <div className="message" key={index}>
+                <img src={person} alt="Person" />
+                <div className="message_content">
+                  <h3>{isAdminMessage(message) ? 'Doctor' : `${message.account.slice(0, 6)}...${message.account.slice(-4)}`}</h3>
+                  <p>{message.text}</p>
+                </div>
+              </div>
+            ))}
+        <div ref={messageEndRef} />
       </div>
-
-
+      <form onSubmit={sendMessage}>
+        {currentChannel && account ? (
+          <input type="text" value={message} placeholder={`Message #${currentChannel.name}`} onChange={(e) => setMessage(e.target.value)} />
+        ) : (
+          <input type="text" value="" placeholder={`Please Connect Wallet / Join the Channel`} disabled />
+        )}
+        <button type="submit">
+          <img src={send} alt="Send Message" />
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default Messages;
